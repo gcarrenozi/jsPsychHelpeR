41964e04f33803fecf16dee1a11ff80833341de6
Gabriel <gcarreno@ieee.org>
2025-06-06 12:51:10 -0400

    Hic eun cambio insignificante

--- Algunas_cositas_para_recordar.R
+++ Algunas_cositas_para_recordar.R
@@ -10,5 +10,5 @@ usethis::pr_push() #con este empujamos el "pull request" en github, lo creamos e
 # Reocrdar el cómo clonar dede github...en new project.
 
 #> Es una buena opción trabajar en dev local y luego al subirla, hacerlo a dev
-#> remote del origen. ddsamkadskljdaskldasjkld
+#> remote del origen.
 
